d5bb7b4dc6717ae14ea2f8030fe43351c6380f56
ABDALLAH7-dot <aalheete5@gmail.com>
2026-07-13 15:28:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,28 @@ from telegram.ext import (
 from config import BOT_TOKEN, CITY, COUNTRY
 
 USERS_FILE = "users.json"
+def load_users():
+    try:
+        with open(USERS_FILE, "r") as file:
+            return json.load(file)
+    except:
+        return []
+
+
+def save_users(users):
+    with open(USERS_FILE, "w") as file:
+        json.dump(users, file)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    users = load_users()
+
+    if chat_id not in users:
+        users.append(chat_id)
+        save_users(users)
+
+    await update.message.reply_text(
+        "✅ تم الاشتراك بنجاح.\n"
+        "سيتم إرسال تذكير عند كل أذان لمدينة هيت."
+    )
